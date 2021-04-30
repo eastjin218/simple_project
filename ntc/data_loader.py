74b7b86ec837941c12e105b6d3fd943016c9a31f
Lee <59238298+eastjin218@users.noreply.github.com>
2021-04-30 09:22:13 +0900

    no.4

--- ntc/data_loader.py
+++ ntc/data_loader.py
@@ -15,7 +15,11 @@ class DataLoader(object):
 
         super().__init__()
 
-        self.
+        self.label = data.Field(
+            sequential=False,
+            use_vocab=True,
+            unk_token=None
+        )
 
         self.text= data.Field(
             use_vocab = True,
@@ -23,4 +27,25 @@ class DataLoader(object):
             include_lengths =False,
             eos_token='<EOS>' if use_eos else None,
         )
+
+        train, valid = data.TabularDataset(
+            path =train_fn,
+            format = 'tsv',
+            field=[
+                ('label', self.label),
+                ('text', self.text),
+            ],
+        ).split(split_ratio=(1 -valid_radio))
+
+        self.train_loader, self.valid_loader =data.BucketIterator.splits(
+            (train, valid),
+            batch_size = batch_size,
+            device = 'cuda : %d'%device if device >= 0 else 'cpu',
+            shuffle=shuffle,
+            sort_key = lambda x : len(x.text),
+            sort_within_batch=True, 
+        )
+
+        self.label.build_vocab(train)
+        self.text.build_vocab(train, max_size =max_vocab, min_freq = min_freq)
 
